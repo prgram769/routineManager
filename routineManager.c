b7dcac3c8cc123362f6c5f8bdbb429d6664d38c4
prgram769 <prgram02@gmail.com>
2025-10-19 13:51:50 +0200

    siguiendo el routine manager

--- routineManager.c
+++ routineManager.c
@@ -7,40 +7,51 @@ properly, you will get a "reward"*/
 
 int main(void) {
     __int8_t option = 1;
-    __int8_t whileComprobation = 0;
+    __int8_t whileComprobation = 1;
 
     char myTask[512];
-    int hoursToCompleteTask;
+    __int8_t hoursToCompleteTask;
+    __int8_t breaks;
+    __int8_t hour;
+    __int8_t minutes;
 
     printf("You must enter a short name for your task: \n");
 
     fgets(myTask, sizeof(myTask), stdin);
     
-    printf("\nRight, now, you must enter the time that you will use to do that task: \n");
-
-    if (scanf("%d", &hoursToCompleteTask) == 1)
-    {
-        printf("\nok\n");
-    } else {
-        printf("Error: You must enter valid data.\n");
-
-        whileComprobation = 1;
-    }
-    
     while (whileComprobation == 1)
     {
-        printf("\nRight, now, you must enter the time that you will use to do that task: \n");
+        printf("\nRight, now, you must enter the breaks: \n");
 
-        if (scanf("%d", &hoursToCompleteTask) == 1)
+        if (scanf("%d", &breaks) == 1)
         {
             printf("\nok\n");
 
-            whileComprobation = 0;
+            continue;
         } else {
             printf("Error: You must enter valid data.\n");
-
-            whileComprobation = 1;
         }
+
+        printf("\nRight, now, you must enter the time that you will use to do that task: \n");
+
+        if (scanf("%d", &hoursToCompleteTask))
+        {
+            printf("\nok");
+
+            continue;
+        } else {
+            printf("Error: You must enter valid data.\n");
+        }
+        
+        printf("\nRight, now, you must enter the digital time at you want finish, first you enter the hour and then the minutes: \n");
+
+        if (scanf("%d %d", &hour, &minutes) == 2)
+        {
+            
+        } else {
+
+        }
+        
     }
     
 }
